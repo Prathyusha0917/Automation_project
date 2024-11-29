89319b41bf96042ef8e1a8e3d5db340ef7eb6856
Prathyusha0917 <prathyusha0917@gmail.com>
2024-11-29 16:18:10 +0530

    Practice

---
+++ src/main/java/com/qt/page/Pythonpage.java
@@ -0,0 +1,10 @@
+package com.qt.page;
+
+public class Pythonpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Prathyusha");
+	}
+
+}
---
+++ src/test/java/com/qt/tests/Pythontest.java
@@ -0,0 +1,5 @@
+package com.qt.tests;
+
+public class Pythontest {
+
+}
